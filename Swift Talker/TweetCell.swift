72b73b7ba644cade17dcea7a965dd10ab4a0c014
Waghmare, Amol <Amol_Waghmare@intuit.com>
2017-04-16 21:47:54 -0700

    Tweet

---
+++ Swift Talker/TweetCell.swift
@@ -0,0 +1,24 @@
+//
+//  TweetCell.swift
+//  Swift Talker
+//
+//  Created by Waghmare, Amol on 16/04/17.
+//  Copyright © 2017 Waghmare, Amol. All rights reserved.
+//
+
+import UIKit
+
+class TweetCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
